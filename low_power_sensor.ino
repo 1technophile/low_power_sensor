e8b2ac40386c32e25eaa274cea8eb2a40d74f956
1technophile <florian.robert@gmail.com>
2016-08-10 15:53:44 +0200

    add disclaimer

--- low_power_sensor.ino
+++ low_power_sensor.ino
@@ -1,3 +1,33 @@
+/*
+  Low power node  - ATMega328p program to send humidity, temperature, hygrometry and battery voltage
+ 
+  This program enables to send sensor data with low power:
+ - send sensor data to a 433Mhz gateway
+ - with 3 AA batteries measured at 5V the current consumption in sleep mode is around 5uA
+  Contributors:
+  - 1technophile
+  Based on the libraries:
+  - RCSwitch
+  - LowPower
+  Based on the work of:
+  Nick Gammon : http://www.gammon.com.au/power
+  Rocketscream: https://github.com/rocketscream/Low-Power
+  Tinkerit: https://code.google.com/archive/p/tinkerit/wikis/SecretVoltmeter.wiki
+  Documentation:
+  Project home: https://github.com/1technophile/low_power_sensor
+  Blog: http://1technophile.blogspot.fr/2016/08/low-cost-low-power-6ua-garden-433mhz.html
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "DHT.h"
 #include "LowPower.h"
 #include <RCSwitch.h>
@@ -5,8 +35,6 @@
 
 #define DHTTYPE DHT11
     
-
-
 RCSwitch mySwitch = RCSwitch();
 
 //Time used to wait for an interval before resending data
